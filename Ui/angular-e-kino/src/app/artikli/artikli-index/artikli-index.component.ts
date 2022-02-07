00c7316391e16a8fc25a60dfbd360690310b2234
Adem Halilović <halilovicadem@gmail.com>
2022-02-07 13:58:40 +0100

    angular src

---
+++ Ui/angular-e-kino/src/app/artikli/artikli-index/artikli-index.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artikli-index',
+  templateUrl: './artikli-index.component.html',
+  styleUrls: ['./artikli-index.component.css']
+})
+
+export class ArtikliIndexComponent implements OnInit {
+
+  
+  constructor() { }
+ 
+
+  ngOnInit(): void {
+  }
+
+}
